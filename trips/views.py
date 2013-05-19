cf0a33a041197ac3b63627c963bb712ee0080a8c
Elliott Eggleston <ejegg.com>
2013-05-19 19:07:09 -0400

    UI for picking tracks

--- trips/views.py
+++ trips/views.py
@@ -30,9 +30,11 @@ def index(request, trackNum="0"):
             'spd': "{0} mph".format(pos.speed * 2.2369362920544)
         }
     allString = json.dumps(map(basics, allPos))
+    allTrips = json.dumps(map(lambda t: {'name': t.name, 'id': t.id}, trips))
     context = Context({
-        'latest' : selectedTrip,
+        'selectedTrip' : selectedTrip,
         'allPos' : allString,
+        'trips' : trips,
         'lastFmUrl' : settings.LAST_FM_URL,
         'mapsKey' : settings.GOOGLE_API_KEY
     })
