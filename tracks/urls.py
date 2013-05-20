e594d9b5bea817f1c549bf0863612f76d7c0931a
Elliott Eggleston <ejegg.com>
2013-05-20 00:18:45 -0400

    Streaming audio!

--- tracks/urls.py
+++ tracks/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from tracks import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<artist>[^/]+)/(?P<title>[^/]+)$', views.lookup, name='lookup')
+    url(r'^(?P<artist>[^/]+)/(?P<title>[^/]+)$', views.lookup, name='lookup'),
+    url(r'^(?P<trackId>\d+)$', views.play, name='play')
 ) 
--- tracks/views.py
+++ tracks/views.py
@@ -1,17 +1,32 @@
 from django.http import HttpResponse
 from django.conf import settings
 from django.db.models import Q
+from django.core.servers.basehttp import FileWrapper
+
+import mimetypes
 import json
 
 from tracks.models import Track
 
 def lookup(request, artist="", title=""):
-    matchingTracks = Track.objects.filter(Q(id3artist__icontains = artist) | Q(id3v2artist__icontains = artist),
+    matchingTracks = Track.objects.filter(Q(id3artist__iexact = artist) | Q(id3v2artist__iexact = artist),
+                                          Q(id3title__iexact = title) | Q(id3v2artist__iexact = title))
+    if len(matchingTracks) == 0:
+        matchingTracks = Track.objects.filter(Q(id3artist__icontains = artist) | Q(id3v2artist__icontains = artist),
                                           Q(id3title__icontains = title) | Q(id3v2artist__icontains = title))
     if len(matchingTracks) == 0:
-        return HttpResponse("[]")
+        return HttpResponse("-1")
     
-    track = matchingTracks[0]
-    path = "file:///mp3s/{0}/{1}/{2}".format(track.album.artist.name, track.album.name, track.filename)
-    
-    return HttpResponse(json.dumps(path))
+    return HttpResponse(matchingTracks[0].id)
+
+def play(request, trackId):
+    tracks = Track.objects.filter(pk=trackId)
+    if len(tracks) == 0:
+        return HttpResponse("")
+    track = tracks[0]
+    path = "/mp3s/{0}/{1}/{2}".format(track.album.artist.name, track.album.name, track.filename)
+    wrapper = FileWrapper(open(path, "rb"))
+    response = HttpResponse(wrapper, content_type='audio/mpeg')
+    response['Content-Length'] = track.size
+    response['Content-Disposition'] = "attachment; filename=" + track.filename
+    return response
